0550c37a3295ff04dc364a704398b4285eb449ab
Lou Bichard <contact@louisjohnbichard.co.uk>
2019-07-15 14:07:49 +0100

    Updated

--- alias.sh
+++ alias.sh
@@ -1,12 +1,22 @@
-alias loujaybee-cool="echo Superrrrr cool"
-
-alias loujaybee-docker-remove-all="
+# Docker stuff
+alias loujaybee-docker-clean-all-images="docker rmi (docker images -q)"
+alias loujaybee-docker-clean-all-processes="docker rm (docker ps -a -q)"
+alias loujaybee-docker-clean-all="
     docker rm (docker ps -a -q) \
     docker rmi (docker images -q)
 "
 
+# Git stuff
+alias loujaybee-git-get-branch-name="git status | grep 'On branch' | awk '{print \$3}' | pbcopy"
+
+# Misc
+alias loujaybee-cool="echo Superrrrr cool"
+
+# Add entries here
 alias loujaybee-list="
 echo All registered CLI: \n\
-- loujaybee-docker-remove-all\n\
+- loujaybee-docker-clean-all\n\
+- loujaybee-docker-clean-all-images\n\
+- loujaybee-docker-clean-all-processes\n\
 - loujaybee-cool\n\
 "
